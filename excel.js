cf4590674a28c3e426f9a806a93c73f3b8377da1
Kuba Tuta <tuta.jakub@gmail.com>
2023-09-28 19:39:50 +0200

    key: value pairs

--- excel.js
+++ excel.js
@@ -1,30 +1,53 @@
 let selectedFile;
 console.log(window.XLSX);
-document.getElementById('input').addEventListener("change", (event) => {
-    selectedFile = event.target.files[0];
-})
+document.getElementById("input").addEventListener("change", (event) => {
+  selectedFile = event.target.files[0];
+});
 
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
-}]
+let data = [
+  {
+    name: "jayanth",
+    data: "scd",
+    abc: "sdef",
+  },
+];
 
+document.getElementById("button").addEventListener("click", () => {
+  XLSX.utils.json_to_sheet(data, "out.xlsx");
+  if (selectedFile) {
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      let data = event.target.result;
+      let workbook = XLSX.read(data, { type: "binary" });
+      console.log(workbook);
+      let resultArray = [];
 
-document.getElementById('button').addEventListener("click", () => {
-    XLSX.utils.json_to_sheet(data, 'out.xlsx');
-    if(selectedFile){
-        let fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-         let data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
+      workbook.SheetNames.forEach((sheet) => {
+        let worksheet = workbook.Sheets[sheet];
+
+        let range = XLSX.utils.decode_range(worksheet["!ref"]);
+        let rows = range.e.r;
+
+        for (let i = 1; i <= rows; i++) {
+          let cellB = worksheet[XLSX.utils.encode_cell({ r: i, c: 0 })];
+          let cellA = worksheet[XLSX.utils.encode_cell({ r: i, c: 1 })];
+          if (cellA && cellB) {
+            let key = cellA.v;
+            let value = cellB.v;
+            let obj = {};
+            obj[key] = value;
+            resultArray.push(obj);
+          }
         }
-    }
+      });
+
+      console.log(resultArray);
+      document.getElementById("jsondata").innerHTML = JSON.stringify(
+        resultArray,
+        undefined,
+        4
+      );
+    };
+  }
 });
